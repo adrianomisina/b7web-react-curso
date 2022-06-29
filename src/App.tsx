68adf6b6b66e6b9586fcdd9d5ccb43daad5a09be
Adriano Misina <adriano.misina@gmail.com>
2022-06-29 17:14:20 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Container, Botao} from "./AppStyles"
+import * as C from "./AppStyles"
+
+const App = () => {
+  return (
+    <C.Container>
+      Texto do Componente
+      <C.Botao>Clique Aqui</C.Botao>
+    </C.Container>
+  )
+}
+
+export default App
